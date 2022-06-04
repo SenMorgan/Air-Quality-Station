3ec77e3bd452a6447e16a03c391b65f29a1390e6
Sen Morgan <technars@gmail.com>
2022-06-05 00:44:10 +0200

    feat: Publishng data to MQTT broker

---
+++ lib/defs/def.h
@@ -0,0 +1,33 @@
+/**
+ * @file def.h
+ * @author SenMorgan https://github.com/SenMorgan
+ * @date 2022-06-05
+ *
+ * @copyright Copyright (c) 2021 Sen Morgan
+ *
+ */
+
+#ifndef _DEF_H_
+#define _DEF_H_
+
+// MQTT definitions
+#define DEFAULT_TOPIC             "/air-quality-station/"
+#define MQTT_WILL_TOPIC           DEFAULT_TOPIC "availability"
+#define MQTT_QOS                  1
+#define MQTT_RETAIN               1
+#define MQTT_WILL_MESSAGE         DEFAULT_TOPIC "offline"
+#define MQTT_SUBSCRIBE_TOPIC      DEFAULT_TOPIC "#"
+#define MQTT_PUBLISH_TOPIC        DEFAULT_TOPIC "state"
+#define MQTT_AVAILABILITY_TOPIC   DEFAULT_TOPIC "availability"
+#define MQTT_AVAILABILITY_MESSAGE "online"
+#define MQTT_UPTIME_TOPIC         DEFAULT_TOPIC "uptime"
+
+// Reconnecting to MQTT server delay
+#define RECONNECT_DELAY     5000
+
+// IO pins
+#define STATUS_LED D4
+#define SDA_PIN    4
+#define SCL_PIN    5
+
+#endif // _DEF_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
+#include <PubSubClient.h>
 #include <EEPROM.h>
 #include "bsec.h"
 
 #include "credentials.h"
+#include "def.h"
+
 const char ssid[] = WIFI_SSID;
 const char pass[] = WIFI_PASSWD;
 
@@ -23,13 +28,18 @@ const uint8_t bsec_config_iaq[] = {
 #include "config/generic_33v_3s_4d/bsec_iaq.txt"
 };
 
-#define STATE_SAVE_PERIOD	UINT32_C(360 * 60 * 1000) // 360 minutes - 4 times a day
+#define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000) // 360 minutes - 4 times a day
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 // Helper functions declarations
 void checkIaqSensorStatus(void);
 void errLeds(void);
 void loadState(void);
 void updateState(void);
+uint8_t reconnect(void);
+void publish_data(void);
 
 // Create an object of the class Bsec
 Bsec iaqSensor;
@@ -41,155 +51,259 @@ String output;
 // Entry point for the example
 void setup(void)
 {
-  EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1); // 1st address for the length
-  Serial.begin(115200);
-  Wire.begin();
+    EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1); // 1st address for the length
+    Serial.begin(115200);
+    Wire.begin(SDA_PIN, SCL_PIN);
 
-  iaqSensor.begin(BME680_I2C_ADDR_SECONDARY, Wire);
-  output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
-  Serial.println(output);
-  checkIaqSensorStatus();
+    iaqSensor.begin(BME680_I2C_ADDR_SECONDARY, Wire);
+    output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
+    Serial.println(output);
+    checkIaqSensorStatus();
 
-  iaqSensor.setConfig(bsec_config_iaq);
-  checkIaqSensorStatus();
+    iaqSensor.setConfig(bsec_config_iaq);
+    checkIaqSensorStatus();
 
-  loadState();
+    loadState();
+    bsec_virtual_sensor_t sensorList[10] = {
+        BSEC_OUTPUT_RAW_TEMPERATURE,
+        BSEC_OUTPUT_RAW_PRESSURE,
+        BSEC_OUTPUT_RAW_HUMIDITY,
+        BSEC_OUTPUT_RAW_GAS,
+        BSEC_OUTPUT_IAQ,
+        BSEC_OUTPUT_STATIC_IAQ,
+        BSEC_OUTPUT_CO2_EQUIVALENT,
+        BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+        BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+        BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+    };
 
-  bsec_virtual_sensor_t sensorList1[2] = {
-    BSEC_OUTPUT_RAW_GAS,
-    BSEC_OUTPUT_IAQ,
-  };
+    iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+    checkIaqSensorStatus();
 
-  iaqSensor.updateSubscription(sensorList1, 2, BSEC_SAMPLE_RATE_ULP);
-  checkIaqSensorStatus();
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+    WiFi.hostname(HOSTNAME);
 
-  bsec_virtual_sensor_t sensorList2[5] = {
-    BSEC_OUTPUT_RAW_TEMPERATURE,
-    BSEC_OUTPUT_RAW_PRESSURE,
-    BSEC_OUTPUT_RAW_HUMIDITY,
-    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
-    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
-  };
+    // Arduino OTA initializing
+    ArduinoOTA.setHostname(OTA_HOSTNAME);
+    ArduinoOTA.setPassword(OTA_PASSWORD);
+    ArduinoOTA.begin();
+    ArduinoOTA.onProgress([](uint16_t progress, uint16_t total)
+                          { digitalWrite(STATUS_LED, !digitalRead(STATUS_LED)); });
+    ArduinoOTA.onEnd([]()
+                     { digitalWrite(STATUS_LED, 1); });
 
-  iaqSensor.updateSubscription(sensorList2, 5, BSEC_SAMPLE_RATE_LP);
-  checkIaqSensorStatus();
+    // Make sure that status led is off
+    digitalWrite(STATUS_LED, 1);
 
-  // Print the header
-  output = "Timestamp [ms], raw temperature [°C], pressure [hPa], raw relative humidity [%], gas [Ohm], IAQ, IAQ accuracy, temperature [°C], relative humidity [%]";
-  Serial.println(output);
+    // MQTT initializing
+    mqttClient.setServer(MQTT_SERVER, MQTT_SERVER_PORT);
+    Serial.println("Connecting to MQTT server...");
+    reconnect();
+
+    // Print the header
+    output = "Timestamp [ms], raw temperature [°C], pressure [hPa], raw relative humidity [%], gas [Ohm], IAQ, IAQ accuracy, temperature [°C], relative humidity [%], Static IAQ, CO2 equivalent, breath VOC equivalent";
+    Serial.println(output);
 }
 
 // Function that is looped forever
 void loop(void)
 {
-  unsigned long time_trigger = millis();
-  if (iaqSensor.run()) { // If new data is available
-    output = String(time_trigger);
-    output += ", " + String(iaqSensor.rawTemperature);
-    output += ", " + String(iaqSensor.pressure);
-    output += ", " + String(iaqSensor.rawHumidity);
-    output += ", " + String(iaqSensor.gasResistance);
-    output += ", " + String(iaqSensor.iaq);
-    output += ", " + String(iaqSensor.iaqAccuracy);
-    output += ", " + String(iaqSensor.temperature);
-    output += ", " + String(iaqSensor.humidity);
-    Serial.println(output);
-    updateState();
-  } else {
-    checkIaqSensorStatus();
-  }
+    ArduinoOTA.handle();
+
+    uint8_t connectedToServer = mqttClient.loop();
+
+    if (iaqSensor.run())
+    { // If new data is available
+        output = String(millis());
+        output += ", " + String(iaqSensor.rawTemperature);
+        output += ", " + String(iaqSensor.pressure);
+        output += ", " + String(iaqSensor.rawHumidity);
+        output += ", " + String(iaqSensor.gasResistance);
+        output += ", " + String(iaqSensor.iaq);
+        output += ", " + String(iaqSensor.iaqAccuracy);
+        output += ", " + String(iaqSensor.temperature);
+        output += ", " + String(iaqSensor.humidity);
+        output += ", " + String(iaqSensor.staticIaq);
+        output += ", " + String(iaqSensor.co2Equivalent);
+        output += ", " + String(iaqSensor.breathVocEquivalent);
+        Serial.println(output);
+        if (connectedToServer)
+        {
+            publish_data();
+        }
+        else
+        {
+            if (reconnect())
+            {
+                publish_data();
+            }
+        }
+
+        updateState();
+    }
+    else
+    {
+        checkIaqSensorStatus();
+    }
+
+    yield();
 }
 
 // Helper function definitions
 void checkIaqSensorStatus(void)
 {
-  if (iaqSensor.status != BSEC_OK) {
-    if (iaqSensor.status < BSEC_OK) {
-      output = "BSEC error code : " + String(iaqSensor.status);
-      Serial.println(output);
-      for (;;)
-        errLeds(); /* Halt in case of failure */
-    } else {
-      output = "BSEC warning code : " + String(iaqSensor.status);
-      Serial.println(output);
+    if (iaqSensor.status != BSEC_OK)
+    {
+        if (iaqSensor.status < BSEC_OK)
+        {
+            output = "BSEC error code : " + String(iaqSensor.status);
+            Serial.println(output);
+            for (;;)
+                errLeds(); /* Halt in case of failure */
+        }
+        else
+        {
+            output = "BSEC warning code : " + String(iaqSensor.status);
+            Serial.println(output);
+        }
     }
-  }
 
-  if (iaqSensor.bme680Status != BME680_OK) {
-    if (iaqSensor.bme680Status < BME680_OK) {
-      output = "BME680 error code : " + String(iaqSensor.bme680Status);
-      Serial.println(output);
-      for (;;)
-        errLeds(); /* Halt in case of failure */
-    } else {
-      output = "BME680 warning code : " + String(iaqSensor.bme680Status);
-      Serial.println(output);
+    if (iaqSensor.bme680Status != BME680_OK)
+    {
+        if (iaqSensor.bme680Status < BME680_OK)
+        {
+            output = "BME680 error code : " + String(iaqSensor.bme680Status);
+            Serial.println(output);
+            for (;;)
+                errLeds(); /* Halt in case of failure */
+        }
+        else
+        {
+            output = "BME680 warning code : " + String(iaqSensor.bme680Status);
+            Serial.println(output);
+        }
     }
-  }
 }
 
 void errLeds(void)
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
 }
 
 void loadState(void)
 {
-  if (EEPROM.read(0) == BSEC_MAX_STATE_BLOB_SIZE) {
-    // Existing state in EEPROM
-    Serial.println("Reading state from EEPROM");
+    if (EEPROM.read(0) == BSEC_MAX_STATE_BLOB_SIZE)
+    {
+        // Existing state in EEPROM
+        Serial.println("Reading state from EEPROM");
 
-    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++) {
-      bsecState[i] = EEPROM.read(i + 1);
-      Serial.println(bsecState[i], HEX);
+        for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++)
+        {
+            bsecState[i] = EEPROM.read(i + 1);
+            Serial.println(bsecState[i], HEX);
+        }
+
+        iaqSensor.setState(bsecState);
+        checkIaqSensorStatus();
     }
+    else
+    {
+        // Erase the EEPROM with zeroes
+        Serial.println("Erasing EEPROM");
 
-    iaqSensor.setState(bsecState);
-    checkIaqSensorStatus();
-  } else {
-    // Erase the EEPROM with zeroes
-    Serial.println("Erasing EEPROM");
+        for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE + 1; i++)
+            EEPROM.write(i, 0);
 
-    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE + 1; i++)
-      EEPROM.write(i, 0);
-
-    EEPROM.commit();
-  }
+        EEPROM.commit();
+    }
 }
 
 void updateState(void)
 {
-  bool update = false;
-  if (stateUpdateCounter == 0) {
-    /* First state update when IAQ accuracy is >= 3 */
-    if (iaqSensor.iaqAccuracy >= 3) {
-      update = true;
-      stateUpdateCounter++;
+    bool update = false;
+    if (stateUpdateCounter == 0)
+    {
+        /* First state update when IAQ accuracy is >= 3 */
+        if (iaqSensor.iaqAccuracy >= 3)
+        {
+            update = true;
+            stateUpdateCounter++;
+        }
     }
-  } else {
-    /* Update every STATE_SAVE_PERIOD minutes */
-    if ((stateUpdateCounter * STATE_SAVE_PERIOD) < millis()) {
-      update = true;
-      stateUpdateCounter++;
-    }
-  }
-
-  if (update) {
-    iaqSensor.getState(bsecState);
-    checkIaqSensorStatus();
-
-    Serial.println("Writing state to EEPROM");
-
-    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE ; i++) {
-      EEPROM.write(i + 1, bsecState[i]);
-      Serial.println(bsecState[i], HEX);
+    else
+    {
+        /* Update every STATE_SAVE_PERIOD minutes */
+        if ((stateUpdateCounter * STATE_SAVE_PERIOD) < millis())
+        {
+            update = true;
+            stateUpdateCounter++;
+        }
     }
 
-    EEPROM.write(0, BSEC_MAX_STATE_BLOB_SIZE);
-    EEPROM.commit();
-  }
+    if (update)
+    {
+        iaqSensor.getState(bsecState);
+        checkIaqSensorStatus();
+
+        Serial.println("Writing state to EEPROM");
+
+        for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++)
+        {
+            EEPROM.write(i + 1, bsecState[i]);
+            Serial.println(bsecState[i], HEX);
+        }
+
+        EEPROM.write(0, BSEC_MAX_STATE_BLOB_SIZE);
+        EEPROM.commit();
+    }
+}
+
+/**
+ * @brief Connecting to MQTT server
+ *
+ * @return true if successfully reconnected to MQTT server, otherwise false
+ */
+uint8_t reconnect(void)
+{
+    if (mqttClient.connect(HOSTNAME, MQTT_LOGIN, MQTT_PASSWORD,
+                           MQTT_WILL_TOPIC, MQTT_QOS, MQTT_RETAIN, MQTT_WILL_MESSAGE))
+    {
+        Serial.println("Successfully connected to " MQTT_SERVER);
+        return 1;
+    }
+    Serial.println("Can't connect to MQTT server...");
+    return 0;
+}
+
+/**
+ * @brief Publish data to server
+ */
+void publish_data(void)
+{
+    static char buff[20];
+
+    mqttClient.publish(MQTT_AVAILABILITY_TOPIC, MQTT_AVAILABILITY_MESSAGE);
+    sprintf(buff, "%ld sec", millis() / 1000);
+    mqttClient.publish(MQTT_UPTIME_TOPIC, buff);
+    sprintf(buff, "%0.2f", iaqSensor.temperature);
+    mqttClient.publish(DEFAULT_TOPIC "temp", buff);
+    sprintf(buff, "%0.2f", iaqSensor.humidity);
+    mqttClient.publish(DEFAULT_TOPIC "humidity", buff);
+    sprintf(buff, "%0.2f", iaqSensor.pressure);
+    mqttClient.publish(DEFAULT_TOPIC "pressure", buff);
+    sprintf(buff, "%0.2f", iaqSensor.iaq);
+    mqttClient.publish(DEFAULT_TOPIC "iaq", buff);
+    sprintf(buff, "%d", iaqSensor.iaqAccuracy);
+    mqttClient.publish(DEFAULT_TOPIC "iaqAccuracy", buff);
+    sprintf(buff, "%0.2f", iaqSensor.co2Equivalent);
+    mqttClient.publish(DEFAULT_TOPIC "co2Equivalent", buff);
+    sprintf(buff, "%0.2f", iaqSensor.breathVocEquivalent);
+    mqttClient.publish(DEFAULT_TOPIC "breathVocEquivalent", buff);
+
+    Serial.println("Data were sent");
 }
