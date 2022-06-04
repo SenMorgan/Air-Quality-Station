023e94afb1812cb15d4c8b9044eaaf90a8581d87
Sen Morgan <technars@gmail.com>
2022-06-04 23:40:39 +0200

    Initial commit

---
+++ lib/defs/credentials.h
@@ -0,0 +1,24 @@
+/**
+ * @file credentials.h
+ * @author SenMorgan https://github.com/SenMorgan
+ *
+ * @copyright Copyright (c) 2022 Sen Morgan
+ *
+ */
+
+#ifndef _CREDENTIALS_H_
+#define _CREDENTIALS_H_
+
+// Replace with your actual WiFi settings
+#define WIFI_SSID   "***"
+#define WIFI_PASSWD "***"
+
+// Replace with your actual MQTT settings
+#define MQTT_SERVER      "***"
+// 1883 - default MQTT port
+#define MQTT_SERVER_PORT 1883
+#define HOSTNAME         "***"
+#define MQTT_LOGIN       "***"
+#define MQTT_PASSWORD    "***"
+
+#endif // _CREDENTIALS_H_
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#include "credentials.h"
+const char ssid[] = WIFI_SSID;
+const char pass[] = WIFI_PASSWD;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
